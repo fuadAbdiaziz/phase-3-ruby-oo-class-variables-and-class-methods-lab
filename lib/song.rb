affac2e3e6f52649ca60457ec31ed8cca8a9734f
fuadAbdiaziz <fuadabdaz99@gmail.com>
2023-06-01 13:14:04 -0700

    done

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+song.count
+
+# => 100
+
+song.artists
+#["OmahLey", "LilMaina" "JayZ"]
+
+song.genres 
+# => [" Pop", "Gengetone" "Rap"]
+
+song.genre_count
+return{"pop"=> 6, "Gengetone => 3" "country" =>}
+
+
+song.artist.count
+# => {"OmahLey"=> 40, "JayZ" =>30 "Lilmaina" =>30}
